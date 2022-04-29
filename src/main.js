d09ad5f4c8099469e341ddf6ecf936971d07da8e
Santiago Lara <oscarescobar773@gmail.com>
2022-04-29 12:37:24 -0500

    Llamar y mostar las categoràs/gèneros de las películas

--- src/main.js
+++ src/main.js
@@ -1,29 +1,65 @@
-import API_KEY from './secrets.js';
+import API_KEY from "./secrets.js";
 
-async function getTrendingMoviesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
-    
+async function getTrendingMoviesPreview() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+  );
+  const data = await res.json();
 
-    const movies = data.results;
+  const movies = data.results;
 
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
-    let fragment = new DocumentFragment();
-    movies.forEach(movie => {
-        
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-container');
+  const trendingPreviewMoviesContainer = document.querySelector(
+    "#trendingPreview .trendingPreview-movieList"
+  );
+  let fragment = new DocumentFragment();
+  movies.forEach((movie) => {
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
 
-        const movieImg = document.createElement('img');
-        movieImg.classList.add('movie-img')
-        movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300/${movie.poster_path}`)
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+    );
 
-        movieContainer.appendChild(movieImg);
-        fragment.appendChild(movieContainer);
-    });
-    trendingPreviewMoviesContainer.appendChild(fragment)
+    movieContainer.appendChild(movieImg);
+    fragment.appendChild(movieContainer);
+  });
+  trendingPreviewMoviesContainer.appendChild(fragment);
 }
 
 getTrendingMoviesPreview();
 
+async function getCategoriesPreview() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY
+  );
+  const data = await res.json();
+
+  const categoriesPreviewContainer = document.querySelector(
+    "#categoriesPreview .categoriesPreview-list"
+  );
+  const fragment = new DocumentFragment();
+
+  const categories = data.genres;
+
+  categories.forEach((category) => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    const categoryTitleText = document.createTextNode(category.name);
+    categoryTitle.setAttribute("id", "id" + category.id);
+    categoryTitle.classList.add("category-title");
+
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    fragment.appendChild(categoryContainer);
+    console.log(category);
+  });
+  categoriesPreviewContainer.appendChild(fragment);
+}
+getCategoriesPreview();
